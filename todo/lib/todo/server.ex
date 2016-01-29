304292ff8a0e1f12d740675433437d5d53b86f96
CoderDennis <dodava@gmail.com>
2016-01-28 21:24:25 -0600

    file system database

--- todo/lib/todo/server.ex
+++ todo/lib/todo/server.ex
@@ -1,12 +1,12 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(todo_list_name) do
+    GenServer.start(Todo.Server, todo_list_name)
   end
 
-  def init(_) do
-    {:ok, Todo.List.new}
+  def init(todo_list_name) do
+    {:ok, {todo_list_name, Todo.Database.get(todo_list_name) || Todo.List.new}}
   end
 
   def add_entry(todo_server, new_entry) do
@@ -25,18 +25,20 @@ defmodule Todo.Server do
     GenServer.call(todo_server, {:entries, date})
   end
 
-  def handle_cast({:add_entry, new_entry}, todo_list) do
-    {:noreply, Todo.List.add_entry(todo_list, new_entry)}
+  def handle_cast({:add_entry, new_entry}, {todo_list_name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, new_entry)
+    Todo.Database.store(todo_list_name, new_state)
+    {:noreply, {todo_list_name, new_state}}
   end
-  def handle_cast({:update_entry, entry_id, updater_fun}, todo_list) do
-    {:noreply, Todo.List.update_entry(todo_list, entry_id, updater_fun)}
+  def handle_cast({:update_entry, entry_id, updater_fun}, {todo_list_name, todo_list}) do
+    {:noreply, {todo_list_name, Todo.List.update_entry(todo_list, entry_id, updater_fun)}}
   end
-  def handle_cast({:delete_entry, entry_id}, todo_list) do
-    {:noreply, Todo.List.delete_entry(todo_list, entry_id)}
+  def handle_cast({:delete_entry, entry_id}, {todo_list_name, todo_list}) do
+    {:noreply, {todo_list_name, Todo.List.delete_entry(todo_list, entry_id)}}
   end
 
-  def handle_call({:entries, date}, _, todo_list) do
-    {:reply, Todo.List.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, {todo_list_name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {todo_list_name, todo_list}}
   end
 end
 
