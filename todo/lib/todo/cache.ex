6c677ab44ced55c30f8eaba76d43e5605ecd48c7
CoderDennis <dodava@gmail.com>
2016-02-05 21:22:49 -0600

    todo distributed

--- todo/lib/todo/cache.ex
+++ todo/lib/todo/cache.ex
@@ -1,37 +1,17 @@
 defmodule Todo.Cache do
-  use GenServer
-
-  def start_link do
-    IO.puts "Starting to-do cache."
-
-    GenServer.start_link(__MODULE__, nil, name: :todo_cache)
-  end
 
   def server_process(todo_list_name) do
     case Todo.Server.whereis(todo_list_name) do
-      :undefined ->
-        GenServer.call(:todo_cache, {:server_process, todo_list_name})
+      :undefined -> create_server(todo_list_name)
       pid -> pid
     end
   end
 
-
-  def init(_) do
-    {:ok, nil}
+  defp create_server(todo_list_name) do
+    case Todo.ServerSupervisor.start_child(todo_list_name) do
+      {:ok, pid} -> pid
+      {:error, {:already_started, pid}} -> pid
+    end
   end
 
-  def handle_call({:server_process, todo_list_name}, _, state) do
-    todo_server_pid =
-      case Todo.Server.whereis(todo_list_name) do
-        :undefined ->
-          {:ok, pid} = Todo.ServerSupervisor.start_child(todo_list_name)
-          pid
-        pid -> pid
-      end
-    {:reply, todo_server_pid, state}
-  end
-
-  # Needed for testing purposes
-  def handle_info(:stop, state), do: {:stop, :normal, state}
-  def handle_info(_, state), do: {:noreply, state}
 end
